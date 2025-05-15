e0683f9d11b9663154f24cb09710924e76e705e5
toth-marcell <toth.marcell.21i@egressy.info>
2025-05-15 17:44:52 +0200

    web: add documentation to books.js

--- web/books.js
+++ web/books.js
@@ -1,6 +1,13 @@
 import { Book } from "./models.js";
 import { Op } from "sequelize";
 
+/**
+ * create a new book in the database
+ * @param {string} author
+ * @param {string} title
+ * @param {string} details
+ * @returns {Promise<string|Book>} error message or newly created book
+ */
 export const newBook = async (author, title, details) => {
   if (!(author && title && details)) return "You must fill out all fields!";
   const book = await Book.create({
@@ -11,6 +18,14 @@ export const newBook = async (author, title, details) => {
   return book;
 };
 
+/**
+ * edit a book in the database based on its id
+ * @param {number} id
+ * @param {string} author
+ * @param {string} title
+ * @param {string} details
+ * @returns {Promise<string|Book>} error message or updated book details
+ */
 export const editBook = async (id, author, title, details) => {
   console.log(author, title, details);
   if (!(author && title && details)) return "You must fill out all fields!";
@@ -21,6 +36,11 @@ export const editBook = async (id, author, title, details) => {
   } else return "No book with that id!";
 };
 
+/**
+ * search for books based on their title
+ * @param {string} search search string
+ * @returns {Promise<Book[]>} list of books whose title contains `search`
+ */
 export const searchBooks = (search) => {
   return Book.findAll({
     order: [["updatedAt", "DESC"]],
