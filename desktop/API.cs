3774c3d160b38490421481f428f35410a7c2691d
toth-marcell <toth.marcell.21i@egressy.info>
2025-05-15 20:57:23 +0200

    web/api, desktop: initial implementation
    desktop now has working login/register/logout and book list

---
+++ desktop/API.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Net.Http;
+
+namespace BookCatalog
+{
+    public static class API
+    {
+        static readonly string URL = "http://localhost:9000/api/";
+        static readonly HttpClient http = new HttpClient();
+        public static string Token = null;
+        public static async Task<List<Book>> GetBooks()
+        {
+            HttpResponseMessage response = await http.GetAsync(URL + "books");
+            response.EnsureSuccessStatusCode();
+            string body = await response.Content.ReadAsStringAsync();
+            List<Book> books = JsonConvert.DeserializeObject<List<Book>>(body);
+            return books;
+        }
+        static async Task<bool> LoginOrRegister(string action, string name, string password)
+        {
+            string body = null;
+            try
+            {
+                string json = JsonConvert.SerializeObject(new LoginBody(name, password));
+                HttpContent reqBody = new StringContent(json, Encoding.UTF8, "application/json");
+                HttpResponseMessage response = await http.PostAsync(URL + action, reqBody);
+                body = await response.Content.ReadAsStringAsync();
+                response.EnsureSuccessStatusCode();
+                TokenBody loginResponse = JsonConvert.DeserializeObject<TokenBody>(body);
+                Token = loginResponse.Token;
+                return true;
+            }
+            catch
+            {
+                MsgResponse msgResponse = JsonConvert.DeserializeObject<MsgResponse>(body);
+                throw new Exception(msgResponse.Message);
+            }
+        }
+        public static async Task<bool> Login(string name, string password) => await LoginOrRegister("login", name, password);
+        public static async Task<bool> Register(string name, string password) => await LoginOrRegister("register", name, password);
+        public static void LogOut() => Token = null;
+    }
+    public struct LoginBody
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        [JsonProperty("password")]
+        public string Password { get; set; }
+        public LoginBody(string name, string password)
+        {
+            Name = name;
+            Password = password;
+        }
+    }
+    public struct TokenBody
+    {
+        [JsonProperty("token")]
+        public string Token { get; set; }
+    }
+    public struct MsgResponse
+    {
+        [JsonProperty("msg")]
+        public string Message { get; set; }
+    }
+}
--- desktop/MainWindow.xaml.cs
+++ desktop/MainWindow.xaml.cs
@@ -23,6 +23,44 @@ namespace BookCatalog
         public MainWindow()
         {
             InitializeComponent();
+            UpdateBooks();
+            NewToolbarButton("Log in", LoginButton_Click);
+        }
+        async void UpdateBooks()
+        {
+            booksPanel.Children.Clear();
+            foreach (Book book in await API.GetBooks())
+            {
+                booksPanel.Children.Add(new BookControl(book));
+            }
+        }
+        Button NewToolbarButton(string text, RoutedEventHandler clickHandler)
+        {
+            Button button = new Button { Content = text };
+            button.Click += clickHandler;
+            toolBar.Children.Add(button);
+            return button;
+        }
+        void LoginButton_Click(object s, RoutedEventArgs e)
+        {
+            new LoginWindow().ShowDialog();
+            if (API.Token == null) LoggedOut();
+            else LoggedIn();
+        }
+        void LogoutButton_Click(object s, RoutedEventArgs e)
+        {
+            API.LogOut();
+            LoggedOut();
+        }
+        void LoggedOut()
+        {
+            toolBar.Children.Clear();
+            NewToolbarButton("Log in", LoginButton_Click);
+        }
+        void LoggedIn()
+        {
+            toolBar.Children.Clear();
+            NewToolbarButton("Log out", LogoutButton_Click);
         }
     }
 }
