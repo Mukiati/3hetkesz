fe9245d09d7fa5dbd1953d34408582c4e0484282
Mukiati <134717594+Mukiati@users.noreply.github.com>
2025-06-10 16:00:37 +0200

    Delete web directory

--- web/maketestdata.js
+++
@@ -1,6 +0,0 @@
-import { faker } from "@faker-js/faker";
-import { newBook } from "./books.js";
-
-for (let i = 0; i < 100; i++) {
-  newBook(faker.book.author(), faker.book.title(), faker.lorem.sentence());
-}
